601227cec910d12ebb78a799f73f30d1bf5b8f5a
Robin Kirkman <robin.kirkman@gmail.com>
2016-08-27 12:23:27 -0400

    update

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,38 @@
  */
 
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <USBAPI.h>
+#include <Wire.h>
 
-void setup() {
+#include <stdint.h>
 
+#include "Adafruit_FONA.h"
+
+#define FONA_RX  9
+#define FONA_TX  8
+#define FONA_RST 4
+#define FONA_RI  7
+
+static SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX);
+// static SoftwareSerial *fonaSerial = &fonaSS;
+
+void setup(void) {
+	fonaSS.begin(4800);
+	Wire.begin(8);
+	Serial.begin(115200);
 }
 
-void loop() {
-
+void loop(void) {
+	bool wait = true;
+	if(Serial.available()) {
+		fonaSS.write(Serial.read());
+		wait = false;
+	}
+	if(fonaSS.available()) {
+		Serial.write(fonaSS.read());
+		wait = false;
+	}
+	if(wait)
+		delay(10);
 }
-
